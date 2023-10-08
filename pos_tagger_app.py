b8d0dc8af148ca4827b1d21202a957988c29fbf8
Arshim <98533618+Arshim28@users.noreply.github.com>
2023-10-08 11:36:35 +0530

    Add files via upload

--- pos_tagger_app.py
+++ pos_tagger_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from main import PosHMM 
+from main import PosHMM
+import re
 
 pos = PosHMM(debug=False)
 cm, possible_tags = pos.train() 
@@ -8,11 +9,13 @@ st.title("POS Tagger Application")
 user_input = st.text_area("Enter a sentence:")
 
 if user_input:
-    pos_tags = pos.predict(user_input)
+    # Remove special characters except for necessary ones
+    user_input_cleaned = re.sub(r'[^a-zA-Z0-9\s!?\'-]', '', user_input)
+    
+    pos_tags = pos.predict(user_input_cleaned)
     st.write("User Input Sentence:")
-    st.markdown(f"> {user_input}")
+    st.markdown(f"> {user_input_cleaned}")
     tagged_sentence = ""
-    for word, tag in zip(user_input.split(), pos_tags):
+    for word, tag in zip(user_input_cleaned.split(), pos_tags):
         tagged_sentence += f'<span style="background-color: white; padding: 4px; border-radius: 4px; color: black;">{word} ({tag})</span> '
     st.markdown(tagged_sentence, unsafe_allow_html=True)
-
